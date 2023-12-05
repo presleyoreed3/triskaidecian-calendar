e044e6e46746c7fc6f093e6a9383d064ef305181
presleyoreed3 <presleyoreed3@gmail.com>
2023-12-05 13:17:50 -0800

    basic auth layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   get "about", to: "about#index"
   get "converter", to: "converter#index"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
 
 end
