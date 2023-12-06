f70feb217fd9f6d7f984c97907e1a4e0cf3326a8
presleyoreed3 <presleyoreed3@gmail.com>
2023-12-06 10:48:19 -0800

    saving users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,19 @@ class RegistrationsController < ApplicationController
 		@user = User.new
 	end
 
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to root_path, notice: "Successfully created your account"
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:email, :password, :passowrd_confimation)
+	end
+
 end
