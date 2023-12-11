3438025f1c1e66e487681fd2b4e01b37e3c28906
presleyoreed3 <presleyoreed3@gmail.com>
2023-12-11 12:03:18 -0800

    fixed issue with user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
-	before_action :set_current_user
+	before_action :set_current_user, if: :logged_in?
+	# skip_before_action :set_current_user, only: [:new, :create]
 
 	def set_current_user
 		if session[:user_id]
@@ -11,5 +12,13 @@ class ApplicationController < ActionController::Base
 	def require_user_logged_in!
 		redirect_to sign_in_path, alert: "You must be signed in to do that" if Current.user.nil?
 	end
-	
+
+	def current_user
+  	Current.user ||= User.find(session[:user_id])
+	end
+
+	def logged_in?
+		current_user
+	end
+
 end
--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -3,11 +3,21 @@ class PasswordsController < ApplicationController
 	before_action :require_user_logged_in!
 
 	def edit
-		
+
 	end
 
 	def update
-		
+		if Current.user.update(password_params)
+			redirect_to root_path, notice: "Your password has been updated."
+		else
+			render :edit
+		end
+	end
+
+	private
+
+	def password_params
+		params.require(:user).permit(:password, :password_confimation)
 	end
 
 end
