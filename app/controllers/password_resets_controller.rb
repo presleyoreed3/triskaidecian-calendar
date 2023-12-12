14f1e95c1ce9b342c6fa2219c52947f3450a2590
presleyoreed3 <presleyoreed3@gmail.com>
2023-12-12 14:06:38 -0800

    fully functional password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,4 +10,26 @@ class PasswordResetsController < ApplicationController
 		end
 		redirect_to root_path, notice: "If we find you in our database you will receive and email from us shortly."
 	end
+
+	def edit 
+		@user = User.find_signed(params[:token], purpose: "password_reset")
+	rescue ActiveSupport::MessageVerifier::InvalidSignature
+		redirect_to sign_in_path, alert: "Your link has expired. Please try again."
+	end
+
+	def update
+		@user = User.find_signed(params[:token], purpose: "password_reset")
+		if @user.update(password_params)
+			redirect_to sign_in_path, notice: "Your password was reset successfully. Please sign in."
+		else
+			render :edit
+		end
+	end
+
+	private
+
+	def password_params
+		params.require(:user).permit(:password, :password_confimation)
+	end
+
 end
